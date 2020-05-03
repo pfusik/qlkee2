766c1e092fc11adc13a55889bf6406206fd54f10
Piotr Fusik <fox@scene.pl>
2020-05-03 23:14:17 +0200

    Small ball.

--- png2gr9.py
+++ png2gr9.py
@@ -1,13 +1,15 @@
+R = 12
+
 def nibble(image, x, y):
-	if (x - 16) **2 + (y - 16) ** 2 >= 225: return 0
+	if (x - R) **2 + (y - R) ** 2 >= (R - 1) ** 2: return 0
 	p = image.getpixel((x, y)) >> 4
 	if p == 0: return 1
 	return p
 
 from PIL import Image
 with Image.open("qlka.png") as image:
-	for y in range(32):
+	for y in range(2 * R):
 		print("\tdta\t", end="")
-		for x in range(0, 32, 2):
-			print("$%02x" % (nibble(image, x, y) << 4 | nibble(image, x + 1, y)), end="\n" if x == 30 else ",")
+		for x in range(0, 2 * R, 2):
+			print("$%02x" % (nibble(image, x, y) << 4 | nibble(image, x + 1, y)), end="\n" if x == 2 * R - 2 else ",")
 
